bb90ccf119b5905873c2c3853e68b95713a39937
muhammadaun90 <57587809+muhammadaun90@users.noreply.github.com>
2022-10-12 02:41:45 +0500

    index.html & script.js for concat in array
    index.html & script.js for concat in array created

---
+++ arrays/Concat/script.js
@@ -0,0 +1,9 @@
+let arr1 = [24,26,30,50,89];
+let arr2 = [33,44,55,66,77];
+// Here we are using concat() method for combining both arrays
+let arr3 = arr1.concat(arr2);
+console.log(arr3);
+
+// We can add a single or comme-separate multiple values too
+let arr4 = arr3.concat(57,92,99,76,43);
+console.log(arr4);
