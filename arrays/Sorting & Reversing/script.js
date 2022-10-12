e8f8b822d072785c171d152bbf83b25887fe44d7
muhammadaun90 <57587809+muhammadaun90@users.noreply.github.com>
2022-10-13 01:07:53 +0500

    sort() & reverse() methods
    index.html & script.js created for sort() & reverse() built-in methods
    
    Sort: It sorts numbers from small to high and strings from A-Z
    
    Reverse: It reverses last element first & first element last. It does'nt matter whether the array is sorted or not; it just reverses the order.

---
+++ arrays/Sorting & Reversing/script.js
@@ -0,0 +1,9 @@
+// Sorting in arrays
+let names = ["Aun", "Shahzad", "Zain", "Rabab", "Tabish"];
+names.sort(); // It sorts names alphabetically
+console.log(names);
+
+// Reversing in arrays
+let foods = ["Biryani", "Pizza", "Tikka", "Sandwich"];
+foods.reverse(); // It reverses from last element to first
+console.log(foods);
