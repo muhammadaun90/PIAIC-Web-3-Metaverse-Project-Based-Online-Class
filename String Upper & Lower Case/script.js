dce97aa3d3b61ac87b5ab23eb621f756ed0e1d3e
muhammadaun90 <57587809+muhammadaun90@users.noreply.github.com>
2022-10-15 08:49:56 +0500

    Converting string to UPPER & lower case
    Created index.html & script.js files for converting string to UPPER & lower case using toUpperCase() & toLowerCase()

---
+++ String Upper & Lower Case/script.js
@@ -0,0 +1,8 @@
+let firstName = "Muhammad";
+let lastName = "auN"
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+let upperName = fullName.toUpperCase();
+console.log(upperName);
+let lowerName = fullName.toLowerCase();
+console.log(lowerName);
