2fb3be757ef85c3fa76de4ea6a3fcfd9b0a259ac
muhammadaun90 <57587809+muhammadaun90@users.noreply.github.com>
2022-10-16 00:26:30 +0500

    Switch Case
    Created index.html & script.js files for switch case.

---
+++ Switch Case/script.js
@@ -0,0 +1,24 @@
+var day = prompt("Enter any day");
+switch(day){
+    case "Monday":
+        console.log("Today is JavaScript onsite class");
+        break;
+    case "Tuesday":
+        console.log("Today is JavaScript online class");
+        break;
+    case "Wednesday":
+        console.log("Today is Python onsite class");
+        break;
+    case "Thursday":
+        console.log("Today is C# onsite class");
+        break;
+    case "Friday":
+        console.log("Today is Python online class");
+         break;
+    case "Saturday":
+        console.log("Today is Web 3.0 onsite class");
+        break;
+    case "Sunday":
+        console.log("Today is Metaverse online class");
+         break;
+}
