90ef5a8ec91ef2431a823f5a7ab7c48ca8b0bc5e
muhammadaun90 <57587809+muhammadaun90@users.noreply.github.com>
2022-10-16 22:18:27 +0500

    Multi Dimensional arrays & Objects in arrays
    Created index.html & script.js files for multi-dimensional arrays & objects in arrays

---
+++ arrays/Multidimensional Arrays/script.js
@@ -0,0 +1,11 @@
+// Taking 3 arrays
+let arr1 = [1,2,3];
+let arr2 = [4,"Karachi","Paksitan"];
+let arr3 = ["Aun", 7, "Ilma University"];
+console.log(arr1,arr2,arr3);
+// Two dimensional arrays
+let arrOfArrays = [arr1,arr2,arr3];
+console.log(arrOfArrays);
+// Multi dimensional arrays
+let arrOfArraysOfArrays = [arrOfArrays,arrOfArrays,arrOfArrays];
+console.log(arrOfArraysOfArrays);
---
+++ arrays/Objects/script.js
@@ -0,0 +1,21 @@
+// Checking array typeof
+let arrCheck = [1,"Aun"];
+console.log("arrCheck type is an ",typeof arrCheck);
+
+// Object in arrays
+let user1 = [{
+    userName: "Aun",
+    Age: 32,
+    Qualification: "BS(CS)",
+},
+{
+    userName: "Daniyal",
+    Age: 28,
+    Qualification: "Masters",
+},
+{
+    userName: "Basit",
+    Age: 27,
+    Qualification: "Bachelors in Software Engineering",
+}];
+console.log(user1);
