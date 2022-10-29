43f30eb8722a188c742bc97a80e467ad39b23953
muhammadaun90 <57587809+muhammadaun90@users.noreply.github.com>
2022-10-29 21:41:56 +0500

    Nested loops practice program
    Created nested loops using for.

---
+++ nested loops/script.js
@@ -0,0 +1,8 @@
+const products = ["Tee Shirts", "Trousers", "Jackets"];
+const gifts = ["Keychain", "Perfume", "Decoration Piece"];
+for(let i = 0; i < products.length; i++){
+    console.log(products[i]);
+    for(let j = 0; j < gifts.length; j++){
+        console.log(gifts[i]);
+    }
+}
