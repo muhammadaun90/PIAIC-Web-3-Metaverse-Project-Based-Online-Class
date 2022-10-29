d4cbf05d223fb79db30ec243d0e34d33ade340ef
muhammadaun90 <57587809+muhammadaun90@users.noreply.github.com>
2022-10-29 21:54:12 +0500

    Loops in object program

---
+++ loops & Objects/script.js
@@ -0,0 +1,11 @@
+// Creating an object variable
+let car = {
+    model: "Yaris",
+    make: "Toyota",
+    year: 2022,
+    color: "Black",
+};
+// Manipulating objects with loops
+for(let key in car){
+    console.log(car[key]);
+}
