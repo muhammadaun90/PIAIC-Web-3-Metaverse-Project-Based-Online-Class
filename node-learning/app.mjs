0dfd9aa992ea1066761198ff9d7341849376c9c2
muhammadaun90 <57587809+muhammadaun90@users.noreply.github.com>
2022-11-02 01:39:42 +0500

    Update app.mjs

--- node-learning/app.mjs
+++ node-learning/app.mjs
@@ -23,3 +23,15 @@ const subtract = (a, b) => {
 }
 const resultSub = subtract(7,2);
 console.log(resultSub);
+
+const arr1 = ["Aun", 1, 2, 5];
+const arr2 = arr1;
+console.log(arr1, arr2);
+
+const greet = (name) => {
+    console.log(`Hello ${name}`);
+};
+const sayHello = (hi, name) => {
+    hi(name);
+};
+hi(greet, 'Aun');
