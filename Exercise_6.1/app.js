cd3b853675e386bc90e486dc821d5f60afba2121
muhammadaun90 <57587809+muhammadaun90@users.noreply.github.com>
2022-11-06 09:38:13 +0500

    Exercise 6.1
    Create a function that takes two parameters, adds the parameters together & output the result using console.log

---
+++ Exercise_6.1/app.js
@@ -0,0 +1,16 @@
+/* 1. Create a function that takes two parameters, adds the parameters together,
+and returns the result.
+2. Set up two different variables with two different values.
+3. Use your function on the two variables, and output the result using
+console.log.
+4. Create a second call to the function using two more numbers as arguments
+sent to the function. */
+
+// Creating a funciton that takes two parameters, adds the parameters together & returns the result
+function add(a,b){
+    return a+b;
+}
+const num1 = 13;
+const num2 = 18;
+console.log(add(num1, num2));
+console.log(add(5,9));
