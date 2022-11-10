39e9bfb609e26d8b5b02d9878a8c8731dfb74f33
muhammadaun90 <57587809+muhammadaun90@users.noreply.github.com>
2022-11-11 02:00:20 +0500

    Arrow functions examples created

---
+++ arrow_functions/app.mjs
@@ -0,0 +1,15 @@
+let addTwo = (a,b) => console.log(a+b);
+addTwo(5,9);
+
+// synthetic sugar passing by arrow function
+let subtract = (x,y) => {
+    return x-y;
+}
+const result = subtract(9,3);
+console.log(result);
+
+// Another example
+const greet = (name) => {
+    console.log(`Hello ${name}`);
+};
+greet('Aun');
