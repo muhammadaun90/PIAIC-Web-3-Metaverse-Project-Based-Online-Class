848bdd1a2203cf5bbfce0e8c51946b6c5dd9f9c6
muhammadaun90 <57587809+muhammadaun90@users.noreply.github.com>
2022-11-11 01:26:50 +0500

    Program done

---
+++ functionsWithTwoParametersButCallingFour/app.mjs
@@ -0,0 +1,8 @@
+// If you call a function with more arguments than parameters, nothing will happen.
+
+function addTwo(a = 25, b = 35) {
+    console.log(a+b);;    
+}
+addTwo();
+// Calling a function with more arguments than parameters
+addTwo(3,6,9,10); // output 3+6 = 9, as addTwo has only two parameters defined in the starting
