90f7cf6ec50a9f7bc60790678e57809139dc03f3
muhammadaun90 <57587809+muhammadaun90@users.noreply.github.com>
2022-11-17 20:46:52 +0500

    Recursive

---
+++ recursive/app.mjs
@@ -0,0 +1,12 @@
+// Recursive function
+function getRecursive(nr) {
+    console.log("Started function iteration: ", nr);
+    if(nr > 0){
+        getRecursive(--nr);
+    }
+    else{
+        console.log("Recursion completed");
+    }
+    console.log("Function ended: ", nr);
+}
+getRecursive(6);
