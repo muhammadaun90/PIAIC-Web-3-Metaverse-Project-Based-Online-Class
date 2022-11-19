47db14656e43a5290b5bc7fbaaa67aa1b568d7d2
muhammadaun90 <57587809+muhammadaun90@users.noreply.github.com>
2022-11-19 23:04:45 +0500

    Update app.mjs

--- SetTimeoutOrderProject/app.mjs
+++ SetTimeoutOrderProject/app.mjs
@@ -1,13 +1,26 @@
-const one = () => console.log('one');
-const two = () => console.log('two');
-const three = () => {
-    console.log('three');
-    one();
-    two();
+/* Use the arrow format to create functions that output the values one and two to the
+console. Create a third function that outputs the value three to the console, and then
+invokes the first two functions.
+Create a fourth function that outputs the word four to the console and also use
+setTimeout() to invoke the first function immediately and then the third function.
+What does your output look like in the console? Try to get the console to output: 
+Four
+Three
+One
+Two
+One
+*/
+
+const One = () => console.log('One');
+const Two = () => console.log('Two');
+const Three = () => {
+    console.log('Three');
+    One();
+    Two();
 }
-const four = () => {
-    console.log('four');
-    setTimeout(one, 0);
-    three();
+const Four = () => {
+    console.log('Four');
+    setTimeout(One, 0);
+    Three();
 }
-four();
+Four();
