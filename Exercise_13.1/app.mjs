aa10da348f768cfe80e056764f7e718d46216898
muhammadaun90 <57587809+muhammadaun90@users.noreply.github.com>
2022-11-22 15:37:17 +0500

    Exercise 13.1

---
+++ Exercise_13.1/app.mjs
@@ -0,0 +1,18 @@
+/* 1. Create a function named greet() that takes one argument, fullName. That
+argument should be an array. Output the items of the array into the console
+interpolated into a greeting message string.
+2. Create a second function that has two arguments: the first one is a string for
+the user's full name, and the second is the callback function.
+3. Split the string into an array using the split() method.
+4. Send the full-name array to the greet() function created in the first step.
+5. Invoke the process of the callback function. */
+
+// setTimeout(() => {
+//     console.log("Hello Aun");
+// }, 5000);
+
+
+// setInterval(() => {
+//     console.log("Hello Aun Zaidi");
+// }, 1000);
+
