ae2908604c7884753932fb8c9effd04a3ac868c1
muhammadaun90 <muhammadaun_90@hotmail.com>
2023-12-16 00:40:37 +0500

    Update script.js
    We created an script.js file & finalized the task given.

---
+++ arrays/Exer Creating Arrays&Obj/script.js
@@ -0,0 +1,13 @@
+let clothes = [];
+clothes.push("Jersey", "Jeans", "Tee Shirt", "Chinos", "Tie");
+console.log(clothes);
+clothes.pop();
+console.log(clothes);
+clothes.push("Dress Pant");
+console.log(clothes);
+console.log(clothes[2]);
+// Creating an empty object literal
+let favCar = {};
+favCar.color = "White";
+favCar.covertible = 100;
+console.log(favCar);
