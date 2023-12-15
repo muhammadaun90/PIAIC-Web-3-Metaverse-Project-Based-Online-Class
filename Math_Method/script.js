a8c985104e4333618d841049d1ebbe327caa3484
muhammadaun90 <muhammadaun_90@hotmail.com>
2023-12-15 21:35:12 +0500

    Math () Method
    Here I created Math.random() and Math.ceil() methods

---
+++ Math_Method/script.js
@@ -0,0 +1,14 @@
+// Math Random
+var decimal  = Math.random();
+console.log(decimal);
+// Log the value of decimal multiplied by 10
+console.log(decimal * 10);
+
+// Math.Ceil()
+// Round up any decimal number to the nearest integer
+var rounded = Math.ceil(0.5);
+console.log(rounded); // result = 1
+var rounded = Math.ceil(1.1);
+console.log(rounded); // result = 2
+var rounded = Math.ceil(2.5);
+console.log(rounded); // result = 3
