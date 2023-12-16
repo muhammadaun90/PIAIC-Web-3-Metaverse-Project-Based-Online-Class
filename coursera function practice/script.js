c685af2061670db73219c1cad8e14e01b8799f2c
muhammadaun90 <muhammadaun_90@hotmail.com>
2023-12-17 00:49:31 +0500

    Update script.js
    Updated script.js with another function program in which I used try and catch

--- coursera function practice/script.js
+++ coursera function practice/script.js
@@ -30,3 +30,22 @@ function letterFinder(word, match) {
     }
 }
 letterFinder("test", "t");
+
+
+// Another example from Coursera
+function addTwoNums(a, b) {
+    try {
+        if (typeof (a) != 'number') {
+            throw new ReferenceError('the first argument is not a number');
+        } else if (typeof (b) != 'number') {
+            throw new ReferenceError('the second argument is not a number');
+        } else {
+            console.log(a+b);
+        }
+    } catch (err) {
+        // console.log(err);
+        console.log("Error!", err);
+    }
+}
+addTwoNums(5,"2");
+console.log("It still works");
