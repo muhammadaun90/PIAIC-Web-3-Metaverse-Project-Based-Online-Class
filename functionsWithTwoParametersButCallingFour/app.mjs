5188cc6d042a380eb14396a2500a7246df39696d
muhammadaun90 <muhammadaun_90@hotmail.com>
2023-12-17 22:04:28 +0500

    Update app.mjs
    New function program created in which I called add function by number & string.

--- functionsWithTwoParametersButCallingFour/app.mjs
+++ functionsWithTwoParametersButCallingFour/app.mjs
@@ -6,3 +6,9 @@ function addTwo(a = 25, b = 35) {
 addTwo();
 // Calling a function with more arguments than parameters
 addTwo(3,6,9,10); // output 3+6 = 9, as addTwo has only two parameters defined in the starting
+
+// Another function program in which we call number with string
+function add(x, y) {
+    console.log(x+y);
+}
+add(7, "7");
