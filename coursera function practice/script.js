ea7a99fd07271b5adb554b29c4997180eee4d26d
muhammadaun90 <muhammadaun_90@hotmail.com>
2023-12-17 21:40:14 +0500

    Update script.js file
    Working again with the 1st program here

--- coursera function practice/script.js
+++ coursera function practice/script.js
@@ -49,3 +49,22 @@ function addTwoNums(a, b) {
 }
 addTwoNums(5,"2");
 console.log("It still works");
+
+// Working again with the above 1st program here
+function letterFinder(word, match) {
+    let condition1 = typeof(word) === "string" && word.length >= 2;
+    let condition2 = typeof(match) === "string" && match.length == 1;
+    if (condition1 && condition2) {
+        for (let i = 0; i < word.length; i++) {
+            if (word[i] == match) {
+                console.log('Found the', match, 'at', i);
+            } else{
+                console.log('---No match found at', i);
+            }
+        }
+    } else {
+        console.log("Please pass correct arguments to the function.");
+    }
+}
+letterFinder(5, 4);
+letterFinder("cat", "c");
