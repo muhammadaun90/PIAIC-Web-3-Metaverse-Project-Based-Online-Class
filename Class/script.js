fe0042e74eacd7326452cafa17e9fa29a7be95fa
muhammadaun90 <muhammadaun_90@hotmail.com>
2024-01-09 23:45:52 +0500

    Update script.js
    OOP Inheritance & polymorphism concepts in JavaScipt.

--- Class/script.js
+++ Class/script.js
@@ -8,3 +8,26 @@ When using a default or empty constructor method, JavaScript implicitly calls th
 class Bird {};
 var myParrot = new Bird();
 console.log(Bird);
+
+/* OOP Principles: Inheritance
+1. There is a base class of "Animal".
+2. There is another class, a sub-class inheriting from "Animal", and the name of this class is "Bird".
+3. Next, there is another class, inheriting from "Bird", and this class is "Eagle".*/
+
+class Animal2 {};
+class Bird2 extends Animal2 {};
+class Sparrow extends Bird2 {};
+
+// OOP Principles: Polymorphism (Multiple forms)
+const bicycle = {
+    bell: function () {
+        return "Ring, ring! Watch out, please."
+    }
+}
+const door = {
+    bell: function () {
+        return "Ring, ring! Come here, please."
+    }
+}
+bicycle.bell();
+door.bell();
