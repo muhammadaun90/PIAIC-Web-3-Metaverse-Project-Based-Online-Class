395b794a96b05d58a184ebae0bc6487c0337270b
muhammadaun90 <muhammadaun_90@hotmail.com>
2024-01-09 22:45:24 +0500

    Functional program created
    1 program for functional programming approach created.

--- functional&oop/script.js
+++ functional&oop/script.js
@@ -1,1 +1,8 @@
 // Functional Programming Approach
+const carPrice = 650000;
+const gSalesTax = 1.2;
+function totalPrice(car, tax) {
+    return car * tax;
+}
+const fullPay = totalPrice(carPrice, gSalesTax);
+console.log(fullPay);
