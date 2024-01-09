0ed2ffc8805183899b3e4d0dd54d382138dd93d2
muhammadaun90 <muhammadaun_90@hotmail.com>
2024-01-09 22:53:09 +0500

    Update script.js
    Another example for functional programming paradigm

--- functional&oop/script.js
+++ functional&oop/script.js
@@ -6,3 +6,13 @@ function totalPrice(car, tax) {
 }
 const fullPay = totalPrice(carPrice, gSalesTax);
 console.log(fullPay);
+
+// Another example for functional programming paradigm
+let currencyOne = 280;
+let currencyTwo;
+let exchangeRate = 1.1;
+function convertCurrency(amount, rate) {
+    return amount * rate;
+}
+currencyTwo = convertCurrency(currencyOne, exchangeRate);
+console.log(currencyTwo);
