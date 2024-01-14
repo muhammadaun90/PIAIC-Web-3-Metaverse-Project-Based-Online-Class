ccb3c9cc8d9eecec2c613f55c7c920c7384df860
muhammadaun90 <muhammadaun_90@hotmail.com>
2024-01-14 15:04:41 +0500

    Update script.js
    Know about default parameters, this example can be improved as follows.

--- Class/script.js
+++ Class/script.js
@@ -94,3 +94,22 @@ console.log(mySQL);
 
 let apple = new String("apple")
 console.log(apple);
+
+class withDefaultParams{
+    constructor(num1 = 1, num2 = 2, num3 = 3, string1 = "Result:", bool1 = true) {
+        this.num1 = num1;
+        this.num2 = num2;
+        this.num3 = num3;
+        this.string1 = string1;
+        this.bool1 = bool1;
+    }
+    calculate() {
+        if(this.bool1) {
+            console.log(this.string1, this.num1 + this.num2 + this.num3);
+            return;
+        }
+        return "The value of bool1 is incorrect"       
+    }
+}
+var better = new withDefaultParams();
+better.calculate();
