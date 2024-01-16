1a6caf2d1e323e9eb6113aec978246a89d07e069
muhammadaun90 <muhammadaun_90@hotmail.com>
2024-01-16 23:53:20 +0500

    Update script.js
    Task exercise completed.

--- OOP _program/script.js
+++ OOP _program/script.js
@@ -109,3 +109,49 @@ cuddles.makeSound(true); // purr, Roar!
 var persianCat = new HouseCat();
 persianCat.furColor = "white";
 console.log("Persian cat fur color: ", persianCat.furColor); // White
+
+
+// Task 1: Code a Person class
+class Person {
+    constructor(name = "Tom", age = 20, energy = 100) {
+        this.name = name;
+        this.age = age;
+        this.energy = energy;
+    }
+    sleep() {
+        this.energy += 10;
+        console.log("Energy is increasing, currently at:", this.energy);
+    }
+    doSomethingFun() {
+        if (this.energy > 0) {
+            this.energy -= 10;
+            console.log("Energy is decreasing, currently at:", this.energy);
+        }
+    }
+}
+// Task 2: Code a Worker class
+class Worker extends Person {
+    constructor(name, age, energy, xp = 0, hourlyWage = 10) {
+        super(name, age, energy);
+        this.xp = xp;
+        this.hourlyWage = hourlyWage;
+    }
+    goToWork() {
+        this.xp += 10;
+        console.log("Experience is increasing: ", this.xp);
+    }
+}
+// Task 3: Code a intern object
+function intern() {
+    let newIntern = new Worker("Bob", 21, 110, 0, 10);
+    newIntern.goToWork();
+    return newIntern;
+}
+intern();
+// Task 4: Code a manager object
+function manager() {
+    let newManager = new Worker("Alice", 30, 120, 100, 30);
+    newManager.doSomethingFun();
+    return newManager;
+}
+manager();
