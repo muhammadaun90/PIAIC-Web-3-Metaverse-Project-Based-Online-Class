2bf134e58234e6f0b1c08e0a19f6205672c215be
muhammadaun90 <muhammadaun_90@hotmail.com>
2024-01-28 22:16:48 +0500

    Update script .js
    You've created an h1 variable in Task 2.
    Now, use that variable to run the addEventListener() method on it.
    Pass two arguments to the addEventListener() method: 'click' and handleClicks

--- Exercise_WebPageContentUpdate/script.js
+++ Exercise_WebPageContentUpdate/script.js
@@ -22,3 +22,34 @@ input.addEventListener("change", function () {
     // console.log(input.value);
     h1.innerText = input.value;
 })
+
+// Another program on console
+var h1 = document.querySelector('h1');
+var arr = [
+    'Example Domain',
+    'First Click',
+    'Second Click',
+    'Third Click'
+];
+function handleClicks() {
+    switch (h1.innerText) {
+        case arr[0]:
+            h1.innerHTML = arr[1];
+            break;
+        case arr[1]:
+            h1.innerHTML = arr[2];
+            break;
+        case arr[2]:
+            h1.innerText = arr[3];
+            break;
+        default:
+            h1.innerText = arr[0]
+            break;
+    }
+}
+/* Add an event listener
+You've created an h1 variable in Task 2.
+Now, use that variable to run the addEventListener() method on it.
+Pass two arguments to the addEventListener() method: 'click' and handleClicks */
+// console.log(h1);
+h1.addEventListener('click', handleClicks);
