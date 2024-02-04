ee8521f04e083d7d6b7edd65601f69233313a489
muhammadaun90 <muhammadaun_90@hotmail.com>
2024-02-04 11:20:26 +0500

    Jest testing

--- testing/app.js
+++ testing/app.js
@@ -2,4 +2,10 @@
 function concatStrings(strA, strB) {
     return strA + strB;
 }
-console.log(concatStrings(123, 456));
+console.log(concatStrings(123, 456)); // Expected output: "123456"
+console.assert(concatStrings("Hello", "World") === "HelloWorld");
+
+function addFive(val) {
+    return val + 5;
+}
+module.exports = addFive;
