6e429787fcc3cab326da588067af172eef741958
muhammadaun90 <muhammadaun_90@hotmail.com>
2024-02-04 22:40:01 +0500

    Unit Testing using jest

--- testing/addFive.test.js
+++ testing/addFive.test.js
@@ -1,5 +1,5 @@
 const {default: TestRunner} = require("jest-runner");
 const addFive = require('./app');
 test('returns the number plus 5', () => {
-    expect(app(1)).toBe(6);
+    expect(addFive(1)).toBe(6);
 });
